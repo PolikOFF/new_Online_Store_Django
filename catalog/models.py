802338131a1dccb62908adbb2ab91b8da7c8d53c
Andrew_Polik <polik.24@mail.ru>
2024-02-07 20:11:06 +0300

    Использована миграция на изменения имени поля в БД Product. Настроено отображение в админке, фильтрация и поиск по полям "имя" и "описание".

--- catalog/models.py
+++ catalog/models.py
@@ -9,12 +9,12 @@ class Product(models.Model):
     description = models.TextField(verbose_name='описание', **NULLABLE)
     image = models.ImageField(verbose_name='изображение', **NULLABLE)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    salary = models.IntegerField(verbose_name='цена', **NULLABLE)
+    price = models.IntegerField(verbose_name='цена', **NULLABLE)
     created_at = models.DateTimeField(verbose_name='дата создания записи в БД')
     updated_at = models.DateTimeField(verbose_name='дата последнего изменения записи в БД')
 
     def __str__(self):
-        return f"{self.name} ({self.category}"
+        return f"{self.name} ({self.category})"
 
     class Meta:
         verbose_name = 'продукт'
