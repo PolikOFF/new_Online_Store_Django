8011a208d3cac12fdca7a4d445a94ec52689fa17
Andrew_Polik <polik.24@mail.ru>
2024-02-17 23:35:27 +0300

    Добавлена статика, определен базовый шаблон, подшаблон. Добавлены контроллеры отображения категорий товаров на главной странице, категорий товаров на отдельной странице, товаров для каждой категории и каждого товара. Сделан шаблонный фильтр для преобразования пути к медиафайлу.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
-from catalog.views import home, contacts
+from catalog.apps import CatalogConfig
+from catalog.views import categories, contacts, category, product_info, product_details
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home),
-    path('contacts/', contacts)
+    path('', categories, name='categories'),
+    path('contacts/', contacts),
+    path('category/', category, name='category'),
+    path('<int:pk>/product_info/', product_info, name='product_info'),
+    path('<int:pk>/product_details/', product_details, name='product_details'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from catalog.models import Category, Product
 
 
-def home(request):
-    return render(request, 'catalog/home.html')
+def categories(request):
+    context = {
+        'object_list': Category.objects.all(),
+        'title': 'SkyStore - Главная'
+    }
+    return render(request, 'catalog/categories.html', context)
 
 
 def contacts(request):
@@ -12,3 +17,28 @@ def contacts(request):
         message = request.POST.get('message')
         print(f'{name} {phone} {message}')
     return render(request, 'catalog/contacts.html')
+
+
+def category(request):
+    context = {
+        'object_list': Category.objects.all(),
+        'title': 'Все категории продуктов'
+    }
+    return render(request, 'catalog/category.html', context)
+
+
+def product_info(request, pk):
+    category_item = Category.objects.get(pk=pk)
+    context = {
+        'object_list': Product.objects.filter(category_id=pk),
+        'title': f'Все продукты категории "{category_item}"'
+    }
+    return render(request, 'catalog/product_info.html', context)
+
+
+def product_details(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        'object': product
+    }
+    return render(request, 'catalog/product_details.html', context)
