fe3de7db6746aa1141f474ffedd9203209520cb6
Andrew_Polik <polik.24@mail.ru>
2024-03-17 00:48:36 +0300

    Созданы приложение users и модель User. Прописаны урлы

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     'catalog',
     'blog',
+    'users',
 
 
 ]
@@ -139,3 +140,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+AUTH_USER_MODEL = 'users.User'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/'
---
+++ users/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from catalog.models import NULLABLE
+
+
+class User(AbstractUser):
+    username = None
+    phone = models.CharField(max_length=50, verbose_name='номер телефона', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватарка', **NULLABLE)
+    country = models.CharField(max_length=100, verbose_name='страна', **NULLABLE)
+    email = models.EmailField(unique=True, verbose_name='email')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
